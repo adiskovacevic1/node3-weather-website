4a92243c5678f8b08c594fb37a278b78665231dd
adiskovacevic <adis.kovacevic@fastsigns.com>
2020-02-03 11:18:24 -0600

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
@@ -19,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     console.log(search.value)
     messageOne.textContent = 'LOADING...'
 
-    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(search.value)).then((response) => {
+    fetch('/weather?address=' + encodeURIComponent(search.value)).then((response) => {
         response.json().then((data) => {
             if(data.error)
             {
