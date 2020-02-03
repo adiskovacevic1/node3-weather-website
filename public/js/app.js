f3363fe88962601a5ccca6c8a08753121d9085d3
adiskovacevic <adis.kovacevic@fastsigns.com>
2020-02-03 10:53:13 -0600

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+console.log('Client side javascript file is loaded!')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    console.log('testing!')
+    console.log(search.value)
+    messageOne.textContent = 'LOADING...'
+
+    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(search.value)).then((response) => {
+        response.json().then((data) => {
+            if(data.error)
+            {
+                messageOne.textContent = data.error
+                return console.log(data.error)
+            }
+
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+            console.log(data.location)
+            console.log(data.forecast)
+        })  
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/3554e8ea8fe0e9d29b95219061d50d03/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude)
+
+    request({ url, json:true}, (error, {body}) =>{
+        if(error) {
+            callback('Unable to connect to the weather service', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            const temp = body.currently.temperature
+            const percip = body.currently.precipProbability
+            callback(undefined, body.daily.data[0].summary + ' It is currenlty ' + temp + ' degrees out. There is a ' + percip + '% chance of rain.' )
+        }
+    })
+}
+
+module.exports = forecast
